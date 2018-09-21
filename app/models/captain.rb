d66ffccfd95fc32c62c9fd88df53a04d1e308ae3
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-09-21 19:49:28 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -10,7 +10,7 @@ class Captain < ActiveRecord::Base
   end
 
   def self.talented_seafarers
-    Captain.joins(boats: :classifications).where(:id => self.sailors.pluck(:id)).includes(classications: {name: "Motorboat"})
+    Captain.joins(boats: :classifications).where(:id => self.sailors.pluck(:id)).includes(boats:{classications: {name: "Motorboat"}})
   end
 
   def self.non_sailors
